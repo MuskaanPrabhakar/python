3fca4a700d543470328faa64aaf37e6f6acb1791
Muskaan <mskn36.r@gmail.com>
2026-06-13 18:36:31 +0000

    practice 2 completed

--- practice 2/wap3.py
+++ practice 2/wap3.py
@@ -1,1 +1,9 @@
 #Write a program that takes a string and uses a dictionary to count the frequency of each character (ignore spaces). Then use that dictionary to print a horizontal bar chart in terminal using * characters. More frequent = longer bar.
+strr = input("Enter words: ")
+strr= strr.split()
+ch= {}
+for word in strr:
+    for char in word:
+        ch[char]=ch.get(char,0)+1
+for char, count in ch.items():
+    print(f"{char}: {'*' * count}")
--- practice 2/wap4.py
+++ practice 2/wap4.py
@@ -1,1 +1,17 @@
 #4. Build a duplicate finder. Take a list of numbers from the user. Use sets to find which numbers appear more than once, which appear exactly once, and which appear more than twice. Print three separate results.
+l = list((input("Enter numbers with spaces: ")).split())
+print(l)
+once = set()
+moreonce = set()
+moretwice= set()
+for num in l:
+    num=int(num)
+    if num not in once:
+        once.add(num)
+    elif num in once and num not in moreonce:
+        moreonce.add(num)
+    elif num in moreonce:
+        moretwice.add(num)
+once -= moreonce
+moreonce-=moretwice
+print(f"Number that appeared more than once are {moreonce}\n Number that appeared only once {once}\n Number that appeared more than twice {moretwice}")
