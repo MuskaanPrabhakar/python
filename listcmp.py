7f0c155072d85adc04d31c0b93c4afea5da88b13
Muskaan <mskn36.r@gmail.com>
2026-07-16 17:30:35 +0100

    Refactor list filtering and add comments
    Commented out list comprehension and modified the loop to remove even numbers.

--- listcmp.py
+++ listcmp.py
@@ -2,7 +2,7 @@ val = [1,2,3,4]
 #for key,num in enumerate(val,0):
     #val[key]= num*2
 #print(val) #list is mutable so the original list is modified
-#new_val= [x for x in val if x%2!=0]
+#new_val= [x for x in val if x%2!=0] #comprehension list
 for num in val:
     if num%2==0:
         val.remove(num)
